8d8e43d0bbadf37b1ce6ad8cc4d1a54c601c0a38
Umaima Malik <umaimam309@gmail.com>
2025-08-22 22:16:22 -0700

    Chapter 1 all Questions !!!....

---
+++ index.js
@@ -0,0 +1,19 @@
+// question no 1 
+alert ("helw world");
+
+// question no 2 
+alert("Error! plese enter a valid pasword ");
+
+// question no 3
+alert ("Welcome to JS Land...\nHappy Coding");
+
+// question 4 
+alert ("Welcome To JS Land");
+alert ("HAPPY CODING! \nPrevent this page from creating additional dialogs.");
+ 
+// question no 5 
+alert("Hellow....Ican run JS through my web browser's console");
+
+// question 6 done 
+alert ("new project");
+
