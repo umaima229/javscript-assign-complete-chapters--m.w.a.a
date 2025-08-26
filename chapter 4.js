df7c166daea8534452b56eced82bbb09e3646851
Umaima Malik <umaimam309@gmail.com>
2025-08-26 10:59:05 -0700

    chapter 4 question solved

---
+++ chapter 4.js
@@ -0,0 +1,26 @@
+// -----------chapter 4 all question--------------
+
+// question no 1
+// var a, b, c
+
+// question no 2
+// LEGAL VARIABLES ------> 
+//   var userName;
+//   var user_age;
+//   var $maliks;
+//   var cast1;
+//   var _maliks;
+//   var $umma;
+ 
+//   matlb ham jsut variable ko  "letters" or  "number"  "$" or "_" just isi sa hi start kar sakty hain bs okay
+   
+// ILLEGAL VARIABLES ----->
+//   var 123name;
+//   var user Name;
+//   var my-name;
+//   var for; 
+//   var alert;
+  
+//   matlb ham variable ko "number sa start" nhi kar sakty or na hi "__" use kr skty or "reserved keywords " bhi use nhi kar skaty means "javascript ka apny words" or "capital word" bhi use nhi kar sakty sapcae ka sath EXAMPLE --> 
+//   var my Name; matlab ya galat ha ya illegal ha 
+//   var myName; or ya sai ha q ka ya legal ha or isy "camel case" bhi kaha jata ha okay 
