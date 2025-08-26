a36be3e6a4ff276f75485190b2b43618ec33b87e
Umaima Malik <umaimam309@gmail.com>
2025-08-26 09:42:07 -0700

    chapter 3 quesions solved

---
+++ chapter3.js
@@ -0,0 +1,22 @@
+// -----------------chapter 3 all questions solved------------------
+
+// question no 1 
+var age = 18;
+alert(15);
+
+// question no 2 
+var value = 20;
+alert ("You Have visited This Site 20 Time");
+
+// question no 3
+var birthyear = 2006;
+document.write("My Birth Year Is 2006 <br>");
+
+// question no 4 
+var visitorname = "Umaima Malik";
+var productname = "  Books"
+var quantity = 10;
+document.write( visitorname + "\nordered" + quantity + "" + productname + "\n on xyz clothing store");
+
+// --------------------// END-------------
+//  -----------lakin mara demag ghoom gya ok------------- 
