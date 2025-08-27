656ba36d8fc98d14e6b81aa373b36d8dabf24abe
Umaima Malik <umaimam309@gmail.com>
2025-08-27 12:10:45 -0700

    chapter 5 half solved

---
+++ chapter5.js
@@ -0,0 +1,43 @@
+// ----------Chapter 5 Questions with Answers-------
+
+// question no 1 
+// --------addition-----
+var num = 3;
+var num1 = 5;
+var num2 = num +num1;
+document.write("Sum off\n" + num + "and\n" + num1 + "is\n" + num2 + "<br>");
+
+// ------------question no 2-------------
+// ----------subtraction-----------------
+var num3 = 8;
+var num4 = 9;
+var num5 = num3 - num4;
+document.write("Sum Off\n" +num3+ "and\n" +num4+ "is\n" +num5 +"<br>" );   
+
+// ---------multiplication------------
+var num6 = 98;
+var num7 = 34;
+var num8 = num6 * num7 ;
+document.write("Sum off\n" +num6+ "and\n" +num7 + "is\n" +num8+ "<br>");
+
+// -------------division----------------
+var num9 =  675.98;
+var num10 = 879.343;
+var num11= num9 / num10;
+document.write("Sum off\n" +num9+ "and\n" +num10+ "is\n" +num11+ "<br>");
+ 
+// -------------modulus-------------
+var num12 = 4567;
+var num13 = 98.454;
+var num14 = 87600;
+var num15 = num12% num13% num14;
+document.write("Sum off\n" +num12+ ",\n" +num13+ "and\n" +num14+ "is\n" +num15+ "<br>" ) ;
+
+// --------------chapter 6 questions solved---------------
+// --------------question no 3-----------------
+
+var a;
+document.write("Value after variable declaration is ; " +a + "<br>");
+
+var b = 5;
+document.write("intialize value ; " +b);
