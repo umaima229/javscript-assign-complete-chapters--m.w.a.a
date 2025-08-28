c9ec76b46251cd984f65cdd7156c2473d7ffd8ef
Umaima Malik <umaimam309@gmail.com>
2025-08-28 11:01:29 -0700

    Chapter 5 done

--- chapter5.js
+++ chapter5.js
@@ -33,11 +33,97 @@ var num14 = 87600;
 var num15 = num12% num13% num14;
 document.write("Sum off\n" +num12+ ",\n" +num13+ "and\n" +num14+ "is\n" +num15+ "<br>" ) ;
 
-// --------------chapter 6 questions solved---------------
-// --------------question no 3-----------------
+// -----------------question no 4 -------------------
 
-var a;
-document.write("Value after variable declaration is ; " +a + "<br>");
+var ticket = 600;
+var calculate= 600*5;
+document.write("Total Cost to buy 5 tickets to a movie is\n" + calculate + "PKR\n" + "<br>" +"<hr>");
 
-var b = 5;
-document.write("intialize value ; " +b);
+// ---------------question no 5------------
+
+var table = 989;
+var result1 = table *1;
+var result2 = table *2;
+var result3 = table *3;
+var result4 = table *4;
+var result5 = table *5;
+var result6 = table *6;
+var result7 = table *7;
+var result8 = table *8;
+var result9 = table *9;
+var result10 = table *10;
+document.write(table+ "*1 = " + result1 + "<br>");
+document.write(table+ "*2 = " + result2 + "<br>");
+document.write(table+ "*3 = " + result3 + "<br>");
+document.write(table+ "*4 = " + result4 + "<br>");
+document.write(table+ "*5 = " + result5 + "<br>");
+document.write(table+ "*6 = " + result6 + "<br>");
+document.write(table+ "*7 = " + result7 + "<br>");
+document.write(table+ "*8 = " + result8 + "<br>");
+document.write(table+ "*9 = " + result9 + "<br>");
+document.write(table+ "*10 = " + result10 +"<br>" +"<hr>");
+// ---------------question no 6------------------
+// ----celsius to farhenhiet-----
+var celsius = 25;
+var farhenhiet = (celsius * 9/5 ) +32;
+document.write(celsius + "°C is equal to" + farhenhiet+ "°F" +"<br>");
+
+// --------farhenhiet to celsius------
+var farhenhiet2 = 98;
+var celsius2 = (farhenhiet2 -32) * 5/9;
+document.write(farhenhiet2 + "°F is equal to" + celsius2.toFixed(2) + "°C" +"<br>" +"<hr>");
+
+// -----------question no 7----
+var item1= 650;
+var item2 = 100;
+var total = item1*3;
+var total1= item2*7;
+var shipping= 100;
+var totalprice = total+ total1+ shipping; 
+document.write("Total Price is\n" +totalprice+ "<br>" +"<hr>" );
+
+// ------------question no 8---------
+var totalmarks = 1000;
+var obtainmarks = 950;
+ var percentage = 1000/950 *100;
+document.write("Student Percentage Is" + percentage+"<br>" +"<hr>");
+
+// ---------question no 9----------
+var usd = 104.80;
+var riyal = 28;
+var multiply = 10*usd;
+var total = 25*riyal;
+var currency = multiply+ total;
+document.write("Currency In PKR " +currency+"<br>" +"<hr>"); 
+
+// --------question no 10------------
+var number = 50;
+var operations = number+5 + number*10 + number/2;
+document.write("Final Answer Is " + operations+ "<br>" +"<hr>");
+
+// ---------------question no 11-------
+
+var currentyear = 2025;
+var birthyear = 2006;
+var calculate= currentyear-birthyear;
+document.write("The CurrentAge is" +calculate +"<br>" +"<hr>")
+
+// -----------------question no 12----------------
+
+var radius = 20;
+// circumference of a circle
+var π = 3.142;
+var circumference= (2*3.142) *2;
+document.write("The Circumference is" + circumference+ "<br>" +"<hr>")
+
+// -------question no 13----------------
+
+var snack = "chocholate";
+var age = 18;
+var maximumage= 80;
+var amount= 2;
+ var lifetimesnacke= ((maximumage-age) *365) *2;
+ document.write( "Need\n" + lifetimesnacke + "\nchocholate to last you until the ripe age of 80" +"<br>"+ "<hr>");
+
+
+//  ============================================END================================================================
---
+++ chapter6.js
@@ -0,0 +1,41 @@
+// --------------chapter 6 questions solved---------------
+// --------------question no 3-----------------
+
+var a;
+document.write("Value after variable declaration is ; " +a + "<br>");
+
+var b = 5;
+ ++b  ;
+document.write("Intialize value ; " +b+ "<br>");
+
+var c = 10;
+++c;
+document.write(" Post-increment value\n" + c + "<br>");
+
+var d = 1898;
+d++;
+document.write(" Pre-increament value is\n " + d + "<br>");
+
+var e = 45389;
+--e;
+document.write("Post-decrement value is\n" + e + "<br>");
+
+var f = 7567573;
+f--;
+document.write("Pre-decrement value is \n" +f+ "<br>");
+
+var g = 5;
+var h = 7;
+var i = ++b + g++ ;
+document.write(" value after addition is \n ; " +i+ "<br>");
+
+var  j = 13;
+--j;
+document.write("Value After Decrement is\n " +j+ "<br>");
+
+
+var  j = 13;
+--j;
+document.write("Value After Decrement is\n " +j+ "<br>" + " Value after decrement The reminder is  " +j%3 + "<br>" +"<hr>");
+
+
