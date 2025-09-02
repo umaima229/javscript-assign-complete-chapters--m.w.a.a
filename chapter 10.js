6a27a23280ed2d42b6f4e853bc152f9ce13a817c
Umaima Malik <umaimam309@gmail.com>
2025-09-01 21:05:18 -0700

    chapter 10 completed

---
+++ chapter 10.js
@@ -0,0 +1,36 @@
+// ------------------------Javascript cHAPTER 10 aLL QUESTIONS sOLVED------------------
+
+// ----------question no 1----------
+var user= prompt("Karachi");
+if (user==="Karachi"){
+    alert("Welcome City Of Light");
+}
+
+// ------question no 2-------
+var gender= prompt("male");
+if (gender==="male"){
+    alert ("GoodMorning Sir");
+    }
+
+ var gender2 = prompt("Female");
+ if (gender2="Female"){
+     alert("GoodMorning Ma'am");
+    }
+
+//   ---------question no 3------------
+var traffic1= prompt("Red");
+if (traffic1==="Red"){
+  alert("Must Stop");
+}
+
+var traffic2= prompt("Yellow");
+if (traffic2==="Yellow"){
+    alert("Ready To Move");
+}
+
+var traffic3= prompt("Green");
+if(traffic3==="Green"){
+    alert("Move Now");
+}
+
+// ===================================================END=============================================================
