1b85b40953ef8d6172828556935e83b510ba6946
Umaima Malik <umaimam309@gmail.com>
2025-09-03 13:06:36 -0700

    Chapter 11 completed

--- javscript-assign-chp-11-to-20-m.w.a.a/chapter 11.js
+++ javscript-assign-chp-11-to-20-m.w.a.a/chapter 11.js
@@ -78,22 +78,105 @@ if (d===true){
 
 //  find grade
 var grade;
+var Remarks;
 if (percentage >= 80){
    grade = "A-one";
+   Remarks = "Excellent";
 }
 else if (percentage >= 70){
    grade = "A";
+   Remarks = "Good";
 }
 else if (percentage >= 60){
    grade = "B";
+   Remarks = "You Nedd to Improve";
 }
 else if (percentage >= 50){
-   grade = "C";
+   grade = "Fail";
+   Remarks  = "Sorry";
 }
-else if (percentage >= 40){
-    grade = "Fail";
-}
+
 document.write("Total Marks: " + totalmarks + "<br>");
 document.write("Obtained Marks: " + obtainmarks + "<br>");
 document.write("Percentage: " + percentage.toFixed(2) + "%<br>");
-document.write("Grade: " + grade);
+document.write("Grade: " + grade +"<br>");
+document.write("Remarks : " + Remarks);
+
+// ----------question no 7-------------
+var secretnumber = 7;
+var guess= +prompt("Guess The Number (1-10:) ");
+if (guess === 7){
+   alert("Bingo Correct Answer");
+}
+else if ( guess + 1 === 7){
+   alert("Close Enough To the Correct Answer ");
+}
+else{
+   alert("Sorry Try AGain");
+}
+
+// --------question no 8 --------
+var num = +prompt("Enter A number");
+if (num % 3 === 0){
+   alert(num + " is divisible by 3");
+}else{
+   alert(num + " is NOt Divisible By 3 ");
+}
+
+// ----------question no 9--------------
+var input = prompt("Enter Even Number AND Odd NUMBER ");
+if (input === "" || isNaN(input)){
+   alert("Invalid Intput");
+}
+else {
+   input = +input;
+  if (input % 2 === 0){
+   alert("Even NUmber");
+}
+else{
+   alert ("Odd Number ");
+}
+}
+
+// ----------question no 10----------------
+var weather = +prompt("Temperature kitna ha ");
+if (weather > 40  ){
+      alert(" 🔥 Its Too Hot Outside");
+}
+ else if(weather > 30 ){
+   alert(" 🌤 Ther Weather Is Today Is Normal ");
+}
+ else if ( weather > 20 ){
+   alert(" 🌥 Today Weather Is Cool ");
+}
+ else if (weather > 10){
+   alert("❄️ OMG! Today's Weather is So Cool");
+}
+else  {
+   alert("No Input Show" );
+}
+// --------------------question no 11-------------------
+var firstnum = +prompt("Enter first number");
+var secondnum = +prompt("Enter Second number");
+var operation = prompt ("Enter Operation (+, -, *, /, %):");
+if ( operation === "+"){
+   alert(firstnum + secondnum );
+}
+else if (operation ===  "-"){
+   alert(firstnum-secondnum);
+}
+else if ( operation === "*"){
+   alert(firstnum*secondnum);
+}
+else if (operation === "/"){
+   alert(firstnum/secondnum);
+}
+else if (operation === "%"){
+   alert(firstnum%secondnum);
+}
+else{
+   alert("No Input");
+}
+
+
+// ===================================================END================================================================
