229f6a07f5c6a3bf9eb6c2e603c381377db6811f
Umaima Malik <umaimam309@gmail.com>
2025-09-05 23:48:51 -0700

    Chapter 13 create Anthor Files

--- chapter 12.js
+++ chapter 12.js
@@ -1,4 +1,4 @@
-// -------------------Javascript All Questions Solved----------------------------------
+// -------------------Javascript  Chapter 12 All Questions Solved----------------------------------
 // ---------question no 1-----------
 
 var ch = prompt("Enter any character (A-Z, a-z, or 0-9)");
@@ -42,62 +42,5 @@ else if (num < 0 ){
 else {
     alert("No Input");
 }
-
-// --------------question no 4-------------
-var letter = prompt("Enter Any Alphabet");
-if( letter === "a" || letter === "e" || letter  === "i" || letter === "o" || letter === "u" ){   
-    alert("small vowel letter");
-}
-else if (letter === "A" || letter === "E" || letter ==="I" || letter === "O" || letter === "U"){
-    alert("Capital vowel");
-}
-else{
-    alert("Not a vowel word");
-}
-
-// -------------question no 5----------
-var pasword = "12345";
-var pasw = prompt("Enter Your Pasword");
-
-if(!pasw){
-    alert("Please Enter Your Pasword");
-}
-else if (pasw === pasword){
-    alert("Correct  Pasword");
-}
-else{
-    alert("Incorrecet Pasword");
-}
-
-// --------------question no 6--------------
-var greeting;
-var hour = 13;
-
-if (hour < 18) {
-   greeting = "Good day";
-}
-else{
-    greeting = "Good evening";
-}
-alert(greeting);
-
-// ------------question no 7-------------
-var time = +prompt("Enter a Time");
-if(time >= 0000 && time  <1200 ){
-      alert("Good morning!");
-}
-else if (time >= 1200 && time < 2100){
-    alert("Good Afternoon");
-}
-else if (time >= 1700 && time < 2100){
-    alert("Good Evening");
-}
-else if (time >= 2100 && time <=2359){
-    alert("Good Night");
-}
-else {
-    alert("Invalid Output");
-}
-
 
 // ==================================================END========================================================================
