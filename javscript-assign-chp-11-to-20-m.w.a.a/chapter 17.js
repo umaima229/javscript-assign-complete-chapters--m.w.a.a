67f5247a71f93ec1112193fb4b91f59c041798af
Umaima Malik <umaimam309@gmail.com>
2025-09-09 10:36:51 -0700

    chapter 17 question no 1 done

---
+++ javscript-assign-chp-11-to-20-m.w.a.a/chapter 17.js
@@ -0,0 +1,9 @@
+// --------------------------------------Javasript chapter 17 All questions Solved------------------------------------------
+
+// question no 1
+var array = ["umaima" , "maliks", ["maliks","saturday"]];
+document.write(array +"<br>" +"<hr>");
+
+// question no 2
+
+
