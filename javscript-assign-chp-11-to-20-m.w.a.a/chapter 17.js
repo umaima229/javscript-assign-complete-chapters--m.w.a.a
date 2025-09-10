39f84d1bd702666a066fcb2752894b39d0135879
Umaima Malik <umaimam309@gmail.com>
2025-09-09 19:41:39 -0700

    CHAPTER 17 DONE

--- javscript-assign-chp-11-to-20-m.w.a.a/chapter 17.js
+++ javscript-assign-chp-11-to-20-m.w.a.a/chapter 17.js
@@ -5,5 +5,12 @@ var array = ["umaima" , "maliks", ["maliks","saturday"]];
 document.write(array +"<br>" +"<hr>");
 
 // question no 2
+var matrix = [[1,2,3],
+            [4,5,6],
+             [7,8,9]];
+document.write(matrix[0] +"<br>");
+document.write(matrix[1] +"<br>");
+document.write(matrix[2] +"<br>" +"<hr>");
 
 
+// =====================================================END==============================================================
