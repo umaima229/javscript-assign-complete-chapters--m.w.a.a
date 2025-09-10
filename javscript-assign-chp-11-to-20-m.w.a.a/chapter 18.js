3e8fd68c4f84a7f7fe9c51ca1a0eb23a5b6f18fb
Umaima Malik <umaimam309@gmail.com>
2025-09-09 21:02:27 -0700

    chapter 18 done

---
+++ javscript-assign-chp-11-to-20-m.w.a.a/chapter 18.js
@@ -0,0 +1,56 @@
+// --------------------------------------------------Chapter 18 All questions Solved------------------------------------------
+
+// Question no 3
+for (var i=0;  i < 10;i++){
+    document.write("Number is " +i+ "<br>");
+}
+document.write("<hr>")
+
+// question no 4
+var input = +prompt("Enter Any number for see the table");
+var length = +prompt("Enter Table Length");
+for (var i = 0 ; i<= length; i++){
+    document.write(input+ "\nx\n" +i+ "=\n"+(input*1)+   "<br>");
+}
+document.write("<hr>");
+
+// Question no 5
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+for (var i=0; i < fruits.length; i++){
+    document.write(fruits[i] +"<br>");
+}
+document.write("<hr>");
+
+
+// question no 6
+// a) Counting numbers
+document.write("<h1> Counting numbers is:</h1>")
+for (var i =0; i < 11; i++){
+    document.write("Counting Numbers Is:"+i+"<br>" );
+}
+
+// b) Reverse Counting
+document.write("<h1> Reverse Counting is:</h1>")
+for (var i=10; i >= 1 ; i--){
+    document.write( " Reverse Counting is: " +i+ "<br>")
+}
+
+// c) Even number is 
+document.write("<h1> Even Number is:</h1>")
+for (var i = 0; i<=20; i +=2){
+    document.write("Even Number Is " +i+"<br>" );
+}
+
+// d)Odd Number 
+document.write("<h1> Odd Number is:</h1>")
+for (var i = 1 ; i<=20 ; i+=2){
+    document.write("Odd Number Is" +i+ "<br>" );
+}
+
+// e)Series with k 
+document.write("<h1> Series k  Number is:</h1>")
+var k  =2 
+for (var i = k ; i<=20 ; i+=k){
+    document.write("Series k  Number Is" +i+ "<br>" );
+}
+
