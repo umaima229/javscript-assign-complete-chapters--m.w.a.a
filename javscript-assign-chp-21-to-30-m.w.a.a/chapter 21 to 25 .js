42c8448a83c9991560c493109220590ee83b161b
Umaima Malik <umaimam309@gmail.com>
2025-09-15 20:30:07 -0700

    5 question Done

---
+++ javscript-assign-chp-21-to-30-m.w.a.a/chapter 21 to 25 .js
@@ -0,0 +1,26 @@
+// Javascript 21 TO 25 All questions Solved
+
+// Question no 1
+var input1 = prompt("Enter Your Firsth Name" );
+var input2 = prompt("Enter Your Second Number");
+var fullname = input1 +""+input2 ;
+document.write(input1+ "\n" +input2 +"<br>" +"<hr>");
+
+// question no 2
+var favrt = prompt("Enter your favrt mobile");
+document.write("your character lenght is\n " + favrt.length +"<hr>" );
+
+// question no 3
+var pak = "Pakistan";
+var index = pak.indexOf("n")
+document.write(" index of n is "+index + "<hr>" );
+
+// question no 4
+var string = "Hellow World";
+var index = string.indexOf("d");
+document.write("Last index of d is" +index  + "<hr>" );
+
+// question no 5
+var pak1 = "pakistani ";
+var charat3=  pak1.charAt(3);
+document.write(" index of 3 is\n" +charat3  + "<hr>" )
