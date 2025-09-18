a8a31819afa5d59360791f0c097b606f90e48b88
Umaima Malik <umaimam309@gmail.com>
2025-09-17 19:29:19 -0700

    1 to 3 questions done

---
+++ javscript-assign-chp-21-to-30-m.w.a.a/Chapter 31 to 34 .js
@@ -0,0 +1,18 @@
+// Javascript Chapter 31 to 34 All question solved 
+
+// question no 1
+var now = new Date();
+document.write(now +"<hr>");
+
+// question no 2
+var now = new Date();
+var month = now.getMonth();
+var date = now.getDate();
+document.write(" Current Month \n" +month +"\n"  );
+document.write("Date\n" +date +"<hr>");
+
+// question no 3
+var now = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var day =  days[now.getDay()];
+document.write("Today day is\n:" +day +"<hr>");
